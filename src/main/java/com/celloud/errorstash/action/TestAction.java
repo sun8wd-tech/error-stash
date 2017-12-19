08245d921a6d5169e73a5c6930c959a168751f96
window sun <sun8wd@gmail.com>
2017-12-19 15:41:12 +0800

    add:添加thymeleaf模板引擎及示例代码

--- src/main/java/com/celloud/errorstash/action/TestAction.java
+++ src/main/java/com/celloud/errorstash/action/TestAction.java
@@ -4,6 +4,8 @@ import com.celloud.errorstash.model.Error;
 import com.celloud.errorstash.service.ErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author <a href="mailto:sunwendong@celloud.cn">sunwd</a>
  * @create 2017/12/19 上午10:50
  */
-@RestController
+@Controller
 @RequestMapping("test")
 public class TestAction {
     @Autowired
     private ErrorService errorService;
 
     @RequestMapping
-    public Page<Error> findAll(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "20") int size) {
-        return errorService.findAll(page >= 1 ? page - 1 : page, size);
+    public String findAll(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "20") int size, Model model) {
+        model.addAttribute("pages", errorService.findAll(page >= 1 ? page - 1 : page, size));
+        return "index";
     }
 }
