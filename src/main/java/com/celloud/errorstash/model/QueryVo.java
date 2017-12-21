05e48f2c7fae467f1d979839fd96ed99d5b46d28
lxj11 <18310678488@163.com>
2017-12-21 15:50:16 +0800

    add:添加了QueryVo.java

---
+++ src/main/java/com/celloud/errorstash/model/QueryVo.java
@@ -0,0 +1,44 @@
+package com.celloud.errorstash.model;
+
+public class QueryVo {
+	private Integer userId;
+	private String module;
+	private String username;
+	private String startTime;
+	private String endTime;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getModule() {
+		return module;
+	}
+	public void setModule(String module) {
+		this.module = module;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	
+	
+	
+
+}
